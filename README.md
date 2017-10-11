1e4d7f10441f9d906caf3d8c711f4cfb36c6f31f
soo-park <sparkstacks@gmail.com>
2017-10-10 23:20:13 -0700

    Response formatted, error handlers added, README updated

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 Build a simple REST-based web server in Scala or Python that supports the following features:
 
 * Respond to requests of the form 
-
 ```“/hello?firstname={first name}&lastname={last name}&gender={m/f}”```
 
 and respond with
@@ -16,7 +15,6 @@ depending on the gender.
 Example: the request “/hello?firstname=tien&lastname=nguyen&gender=m” returns “Hello Mr Tien Nguyen”
 
 * Respond to requests of the form 
-
 ```“/compute?num1={num1}&num2={num2}&operator={add/subtract/multiply/divide}”```
 
 and respond with the result
@@ -24,7 +22,6 @@ and respond with the result
 Example: the request “/compute?num1=5&num2=3&operation=subtract” returns “2” (5-3=2)
 
 * Respond to requests of the form 
-
 ```“/date”```
 
 with the current date in the form “yyyy-mm-dd”
@@ -32,21 +29,17 @@ with the current date in the form “yyyy-mm-dd”
 Example: “/date” returns “2017-09-20”
 
 
-## TO RUN THE APP
+## BASIC INSTRUCTIONS TO RUN THIS FLASK APP
 
-1. Open your terminal, make secret.sh file
-
-Command: echo "export secret_key='abc'" >> secret.sh Reason: you want web frame "Flask" and other requirements to run in your computer to test the code, but you do not want to install everything needed to your global environment. Secret.sh file will contain the key for the app to run.
-
-2. Run your virtual environment
+1. Run your virtual environment
 
 Command:
 
-virtualenv env; source env/bin/activate; source secret.sh
+virtualenv env; source env/bin/activate;
 
 Reason: to make the installation run in a limited space
 
-3. Install requirements
+2. Install requirements
 
 Command: 
 
@@ -54,29 +47,28 @@ pip install -r requirements.txt
 
 Reason: all needed installations are saved in requirements.txt. pip is a command for Python to pick up necessary packages and install them.
 
-4. Run server
+3. Run server
 
 Command: python server.py
 
 Reason: have your server running, so that what you interact with server-client will be viewable on your browser
 
-5. Run your Flask on browser
+4. Run your Flask on browser
 
 Command: localhost:5000 on your browser address bar
 
 Reason: Because your server is running, you can see the site
 
-6. When done, close the server
+5. When done, close the server
 control c
 
-7. If accidently closed the window
+6. If accidently closed the window and getting error when trying to run Python server again
 ps -fA | grep python
 find the second number of the server
 kill (second number)
 
-8. Save environment if dependancies changed
+7. Save environment if dependancies changed
 pip freeze > requirements.txt
 
-9. Deactivate virtual env
+8. Deactivate virtual env
 deactivatae
-
--- server.py
+++ server.py
@@ -3,34 +3,45 @@
 
 """This module is built to run a simple RESTful API"""
 import os
-
-# bringin json
-from json import dumps
-
-# bring in datetime object
 import datetime
-
-# Execute Flask object
-from flask import Flask, jsonify, request, redirect, url_for, make_response, abort
+import numbers
+from json import dumps
+from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response, abort
 
 # from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 
-# "source secret.sh" to run flask server prior to "python server.py"
-# content of the sercret.sh: export secret_key="<your secret key>"
-app.secret_key = os.environ['secret_key']
 
 @app.route('/')
 def index():
-    """root get request"""
+    """
+    [root get request]
+    """
     return "[CONNECTED] available routes: consult README.md", 200
 
-# /hello?firstname={first name}&lastname={last name}&gender={m/f}
-# curl -i http://localhost:5000/hello?firstname=tien&lastname=nguyen&gender=m
-# "Hello Mr Tien Nguyen"
-@app.route('/hello', methods=['GET'])
+
+@app.route('/hello', methods=['GET', 'POST'])
 def say_hello():
-    """say hello with gender, first/last name"""
+    """
+    [Say hello with gender, first/last name]
+    This route will receive three parameters, and will return a greeting.
+
+    Route structure:
+    /hello?firstname={first name}&lastname={last name}&gender={m/f}
+
+    Parameters:
+    firstname, lastname, gender(choose: m/f)
+
+    Response: 
+    "Hello Mr {First Name} {Last Name}”
+    or
+    “Hello Ms {First Name} {Last Name}”
+    depending on the gender.
+
+    Example:
+    request “/hello?firstname=tien&lastname=nguyen&gender=m”
+    response “Hello Mr Tien Nguyen”
+    """
 
     firstname = request.args.get('firstname')[0].upper() + request.args.get('firstname')[1:]
     lastname = request.args.get('lastname')[0].upper() + request.args.get('lastname')[1:]
@@ -46,12 +57,24 @@ def say_hello():
     return dumps("Hello" + " " +prefix + " " + firstname + " " + lastname), 201
 
 
-# /compute?num1={num1}&num2={num2}&operator={add/subtract/multiply/divide}
-# /compute?num1=5&num2=3&operation=subtract
-# returns “2” (5-3=2)
-@app.route('/compute', methods=['GET'])
+@app.route('/compute', methods=['GET', 'POST'])
 def calculate():
-    """calculation"""
+    """
+    [Basic Calculation]
+    This route will receive three parameters and will return the result of calculation.abort
+
+    Route structure: 
+    /compute?num1={num1}&num2={num2}&operator={add/subtract/multiply/divide}
+
+    Parameters:
+    num1, num2, operator(choose: add/subtract/multiply/divide)
+
+    Response:
+    calculated result in an integer
+
+    Example:
+    the request “/compute?num1=5&num2=3&operation=subtract” returns “2” (5-3=2)
+    """
 
     num1 = int(request.args.get('num1').decode('utf-8'))
     num2 = int(request.args.get('num2').decode('utf-8'))
@@ -64,27 +87,58 @@ def calculate():
     elif operation == "multiply":
         result = num1 * num2
     elif operation == "divide":
+        if (num2 == 0):
+            return "Cannot divide with zero"
         result = num1 / num2
     else:
-        return "ERROR: Input incorrect to calculate", 404
+        return "ERROR: Input incorrect to calculate", 500
 
     return dumps(result), 201
 
 
-# /date
-# with the current date in the form “yyyy-mm-dd” eg. “2017-09-20”
 @app.route('/date')
-def date():
-    """date get request"""
-    date_obj = datetime.datetime.now()
+def get_date():
+    """
+    [Get date for today in iso format]
+    This route will not receive any parameters, and will return today's date in iso format
+
+    Route structure:
+    /date
+
+    Response:
+    the current date in the form “yyyy-mm-dd”
+
+    Example:
+    “/date” returns “2017-09-20”
+    """
+    
+    date_obj = datetime.date.today()
+    
     return dumps(date_obj.isoformat()), 200
 
 
+#####################
+#   Error Routes
+#####################  
+
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(e):
     """Better error handling"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return make_response(jsonify({'error': 'Internal server error'}), 500)
+
+@app.errorhandler(403)
+def page_forbidden(e):
+    return make_response(jsonify({'error': 'page_forbidden'}), 403)
+
+
+# To run the error routes above, turn off the debug mode below
+#####################
+#   Context Processors
+#####################
 
 if __name__ == '__main__':
     # debug=True here, since it need be True at point DebugToolbarExtension get invoked
